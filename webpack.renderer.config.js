7938e5bba8a2d7573af1bf37b2dc447b1e7eaac7
Ian McNaughton <IanMcNaugh@gmail.com>
2023-02-15 11:42:23 -0600

    eh a bit of a hack, but the css is copied over now

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,2 +1,11 @@
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: "src/renderer/index.css", to: "index.css" },
+            ],
+        }),
+    ],
 }
